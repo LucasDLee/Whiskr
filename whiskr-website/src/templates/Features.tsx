9198e38dd16a3e54fd4d3948356cb8f411d18d12
Lucas Lee <lucasdlee23@gmail.com>
2024-10-27 16:35:39 -0700

    Setup website

---
+++ whiskr-website/src/templates/Features.tsx
@@ -0,0 +1,148 @@
+import { useTranslations } from 'next-intl';
+
+import { Background } from '@/components/Background';
+import { FeatureCard } from '@/features/landing/FeatureCard';
+import { Section } from '@/features/landing/Section';
+
+export const Features = () => {
+  const t = useTranslations('Features');
+
+  return (
+    <Background>
+      <Section
+        subtitle={t('section_subtitle')}
+        title={t('section_title')}
+        description={t('section_description')}
+      >
+        <div className="grid grid-cols-1 gap-x-3 gap-y-8 md:grid-cols-3">
+          <a href="https://drive.google.com/file/d/18I_Ak6GJKR6-1bF1CcT-RvQJ0V3owQ2b/view" target="_blank" rel="noreferrer noopener">
+            <FeatureCard
+              icon={(
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26">
+                  <polygon className="play-btn__svg" points="9.33 6.69 9.33 19.39 19.3 13.04 9.33 6.69" fill="white" stroke="none" />
+                  <path className="play-btn__svg" d="M26,13A13,13,0,1,1,13,0,13,13,0,0,1,26,13ZM13,2.18A10.89,10.89,0,1,0,23.84,13.06,10.89,10.89,0,0,0,13,2.18Z" fill="white" stroke="none" />
+                </svg>
+              )}
+              title={t('feature1_title')}
+            >
+              {t('feature_description1')}
+            </FeatureCard>
+          </a>
+          {/* <FeatureCard
+            icon={(
+              <svg
+                className="stroke-primary-foreground stroke-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M0 0h24v24H0z" stroke="none" />
+                <path d="M12 3l8 4.5v9L12 21l-8-4.5v-9L12 3M12 12l8-4.5M12 12v9M12 12L4 7.5" />
+              </svg>
+            )}
+            title={t('feature1_title')}
+          >
+            {t('feature_description')}
+          </FeatureCard>
+
+          <FeatureCard
+            icon={(
+              <svg
+                className="stroke-primary-foreground stroke-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M0 0h24v24H0z" stroke="none" />
+                <path d="M12 3l8 4.5v9L12 21l-8-4.5v-9L12 3M12 12l8-4.5M12 12v9M12 12L4 7.5" />
+              </svg>
+            )}
+            title={t('feature2_title')}
+          >
+            {t('feature_description')}
+          </FeatureCard>
+
+          <FeatureCard
+            icon={(
+              <svg
+                className="stroke-primary-foreground stroke-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M0 0h24v24H0z" stroke="none" />
+                <path d="M12 3l8 4.5v9L12 21l-8-4.5v-9L12 3M12 12l8-4.5M12 12v9M12 12L4 7.5" />
+              </svg>
+            )}
+            title={t('feature3_title')}
+          >
+            {t('feature_description')}
+          </FeatureCard>
+
+          <FeatureCard
+            icon={(
+              <svg
+                className="stroke-primary-foreground stroke-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M0 0h24v24H0z" stroke="none" />
+                <path d="M12 3l8 4.5v9L12 21l-8-4.5v-9L12 3M12 12l8-4.5M12 12v9M12 12L4 7.5" />
+              </svg>
+            )}
+            title={t('feature4_title')}
+          >
+            {t('feature_description')}
+          </FeatureCard>
+
+          <FeatureCard
+            icon={(
+              <svg
+                className="stroke-primary-foreground stroke-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M0 0h24v24H0z" stroke="none" />
+                <path d="M12 3l8 4.5v9L12 21l-8-4.5v-9L12 3M12 12l8-4.5M12 12v9M12 12L4 7.5" />
+              </svg>
+            )}
+            title={t('feature5_title')}
+          >
+            {t('feature_description')}
+          </FeatureCard>
+
+          <FeatureCard
+            icon={(
+              <svg
+                className="stroke-primary-foreground stroke-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M0 0h24v24H0z" stroke="none" />
+                <path d="M12 3l8 4.5v9L12 21l-8-4.5v-9L12 3M12 12l8-4.5M12 12v9M12 12L4 7.5" />
+              </svg>
+            )}
+            title={t('feature6_title')}
+          >
+            {t('feature_description')}
+          </FeatureCard> */}
+        </div>
+      </Section>
+    </Background>
+  );
+};
