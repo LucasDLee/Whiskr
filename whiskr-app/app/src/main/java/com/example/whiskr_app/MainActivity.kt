4988cd2803977e2fe0b304f4e74dcf2888a4de09
Lucas Lee <lucasdlee23@gmail.com>
2024-11-06 16:30:03 -0800

    Reorganized and removed unnecessary files

--- whiskr-app/app/src/main/java/com/example/whiskr_app/MainActivity.kt
+++ whiskr-app/app/src/main/java/com/example/whiskr_app/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_catmap, R.id.nav_catbot_all_messages
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- whiskr-app/app/src/main/java/com/example/whiskr_app/ui/catbot/CatbotFragment.kt
+++ whiskr-app/app/src/main/java/com/example/whiskr_app/ui/catbot/CatbotFragment.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.whiskr_app.databinding.CatbotChatBinding
+import com.example.whiskr_app.databinding.FragmentCatbotAllChatsBinding
 
-class SlideshowFragment : Fragment() {
+class CatbotFragment : Fragment() {
 
-    private var _binding: CatbotChatBinding? = null
+    private var _binding: FragmentCatbotAllChatsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,10 +20,7 @@ class SlideshowFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
-
-        _binding = CatbotChatBinding.inflate(inflater, container, false)
+        _binding = FragmentCatbotAllChatsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
 //        val textView: TextView = binding.textSlideshow
