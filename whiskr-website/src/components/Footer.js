12d0710e9733d97666746313ea6a39468584933a
Lucas Lee <lucasdlee23@gmail.com>
2024-11-08 15:27:33 -0800

    New fonts; added public folder

--- whiskr-website/src/components/Footer.js
+++ whiskr-website/src/components/Footer.js
@@ -3,7 +3,7 @@ export const Footer = () => {
   return (
     <div class="footerSection">
       <div class="footerDescription">
-        © Copyright 2024 SaaS Template. Designed by <a class="text-blue-500 hover:text-blue-600" href="https://creativedesignsguru.com">Creative Designs Guru</a>
+        © Copyright 2024 SaaS Template. Designed by <a class="text-blue-500 hover:text-blue-600" href="https://github.com/ixartz/SaaS-Boilerplate">Creative Designs Guru</a>
       </div>
     </div>
   );
--- whiskr-website/src/components/Hero.js
+++ whiskr-website/src/components/Hero.js
@@ -3,7 +3,7 @@ export const Hero = () => {
   return (
 
     <div class="heroSection">
-      <div class="heroTitle">Whiskr</div>
+      <div class="heroTitle" style={{ fontFamily: "Pacifico" }}>Whiskr</div>
       <div class="heroSubtitle">An easier way to adopt a cat</div>
     </div>
   );
