c3df357b5e42afae62c4fb78c0815139056c744d
Lucas Lee <lucasdlee23@gmail.com>
2024-11-22 23:11:43 -0800

    Team cards

--- whiskr-website/src/components/Team.js
+++ whiskr-website/src/components/Team.js
@@ -1,16 +1,53 @@
+import { TeamCard } from "./TeamCard"
+import githubIcon from "../images/github.webp"
+
 export const Team = () => {
 
   const team = [
-    
+    {
+      description: "DESCRIPTION", // small description about what the team member did
+      image: githubIcon,
+      jobs: ["a", "b", "c"], // tasks the team member did
+      name: "PERSON" // team member name
+    },
+    {
+      description: "DESCRIPTION", // small description about what the team member did
+      image: githubIcon,
+      jobs: ["a", "b", "c"], // tasks the team member did
+      name: "PERSON" // team member name
+    },
+    {
+      description: "DESCRIPTION", // small description about what the team member did
+      image: githubIcon,
+      jobs: ["a", "b", "c"], // tasks the team member did
+      name: "PERSON" // team member name
+    },
+    {
+      description: "DESCRIPTION", // small description about what the team member did
+      image: githubIcon,
+      jobs: ["a", "b", "c"], // tasks the team member did
+      name: "PERSON" // team member name
+    }
   ]
 
   return (
     <section className="featuresSection">
       <div className="featureSeparator">
-          <div className="featureHeader">Team</div>
-          <div className="featureSubheader">An Outstanding 4-Person Group</div>
-          <div className="featureDescription">Our innovative 4-person team developed our application from the ground-up.</div>
-        </div>
-      </section>
+        <div className="featureHeader">Team</div>
+        <div className="featureSubheader">An Outstanding 4-Person Group</div>
+        <div className="featureDescription">Our innovative 4-person team developed our application from the ground-up.</div>
+      </div>
+      <div className="teamGrid">
+        {team.map((member, val) => {
+          return <TeamCard
+                  key={val}
+                  description={member.description}
+                  image={member.image}
+                  imageDescription={member.imageDescription}
+                  jobs={member.jobs}
+                  name={member.name} />
+        })}
+      </div>
+    </section>
   )
 }
