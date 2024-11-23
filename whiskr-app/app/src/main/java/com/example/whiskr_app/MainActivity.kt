6dde6151b41b2101e50b9174ce6c7c55a142d48e
lele chen <ciralele@gmail.com>
2024-11-23 03:37:13 -0800

    Add custom toolbar and fix layout problem

--- whiskr-app/app/src/main/java/com/example/whiskr_app/MainActivity.kt
+++ whiskr-app/app/src/main/java/com/example/whiskr_app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -19,14 +20,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Set the custom toolbar
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.custom_toolbar)
+        setSupportActionBar(toolbar)
+
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_adoption, R.id.nav_catmap, R.id.nav_catbot_all_messages
+                R.id.nav_home, R.id.nav_catmap, R.id.nav_catbot_all_messages
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
